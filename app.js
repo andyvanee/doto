cd6a2e6a3add1f157d2541fb5bee07e5292f3448
Andy VanEe <1andyvanee@gmail.com>
2014-10-22 22:29:39 -0600

    Hacking...

---
+++ app.js
@@ -0,0 +1,45 @@
+$(function(){
+  var messages = new Firebase('https://flickering-inferno-3391.firebaseio.com/messages')
+  ,   room
+  ;
+
+  $(window).on('hashchange', function(){
+    var hash = window.location.hash.slice(1)
+    ,   href = window.location.href
+    ;
+
+    if (hash == '') {
+      window.location.hash = '#/default';
+      return;
+    }
+
+    room = messages.child(hash);
+
+    $('#email-to-friend').attr('href', 'mailto:?subject='+encodeURI('Collaborate on my doto list!')+'&body='+encodeURI(window.location.href));
+
+    $('#todos').empty();
+
+    room.on('child_added', function(snapshot) {
+      var todo = snapshot.val();
+      displayTodo(todo.todo, todo.active);
+    });
+
+  }).trigger('hashchange');
+
+
+  $('#todo').keypress(function (e) {
+    if (e.keyCode == 13) {
+      var todo = $('#todo').val();
+      room.push({todo: todo, active: true});
+      $('#todo').val('');
+    }
+  });
+
+  function displayTodo(todo, active) {
+    $('#todos').append($('<li/>').text(todo));
+  };
+
+  window.randomList = function(){
+    window.location.hash = '#/'+Math.random().toString(36).substring(7);
+  }
+});
